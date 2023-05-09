8df751b2d3cab91266caf82dd92b4a52dac04faa
Clément VANDENDAELEN <cv@icure.com>
2023-05-09 17:05:58 +0200

    Code cleanup

--- packages/ehr-lite/marshaller/src/index.ts
+++ packages/ehr-lite/marshaller/src/index.ts
@@ -1,14 +1,9 @@
-import { Project, ts, Type } from "ts-morph";
-import { toJSONGenerator } from "./generators/toJSONGenerator";
-import { fromJSONGenerator } from "./generators/fromJSONGenerator";
-import { DefaultComponent } from "./types/DefaultComponent";
-import { ClassEntityComponent } from "./types/ClassEntityComponent";
-import { SetComponent } from "./types/SetComponent";
-import { ClassComponent } from "./types/ClassComponent";
-import { ArrayComponent } from "./types/ArrayComponent";
-import { MapComponent } from "./types/MapComponent";
-import { ClassBundle } from "./types/ClassBundle";
-import { ArrayBufferComponent } from "./types/ArrayBufferComponent";
+import {Project} from "ts-morph";
+import {toJSONGenerator} from "./generators/toJSONGenerator";
+import {fromJSONGenerator} from "./generators/fromJSONGenerator";
+import {ClassComponent} from "./types/ClassComponent";
+import {ClassBundle} from "./types/ClassBundle";
+import {classComponentFactory} from "./generators/classComponentFactory";
 
 const project = new Project({
   tsConfigFilePath: "../ehr/tsconfig.json",
@@ -18,88 +13,16 @@ const sourceFiles = project.addSourceFilesAtPaths(
   "../ehr/src/models/*.model.ts"
 );
 
-function typeAnalyser(type: Type): ClassComponent {
-  let nullable = false;
-
-  if (type === undefined) {
-    return new DefaultComponent(nullable);
-  }
-
-  if (type.isUnion() && type.isNullable()) {
-    nullable = true;
-    type = type.getNonNullableType();
-  }
-
-  if (type.isClass()) {
-    return new ClassEntityComponent(
-      type.getSymbol()!.compilerSymbol.escapedName!,
-      nullable
-    );
-  }
-
-  if (type.isObject()) {
-    const typeType = type.getSymbol()!.compilerSymbol.escapedName;
-
-    if (typeType === "Set") {
-      return new SetComponent(nullable, [
-        typeAnalyser(type.getTypeArguments()[0]),
-      ]);
-    }
-
-    if (typeType === "Array") {
-      return new ArrayComponent(nullable, [
-        typeAnalyser(type.getTypeArguments()[0]),
-      ]);
-    }
-
-    if (typeType === "Map") {
-      const regex =
-        /Map<\s*((?:string|number|boolean|bigint|symbol|import\("[^"]+"\)\.[^,\s]+))\s*,\s*([\s\S]+)\s*>/;
-      const typeArguments = type.getTypeArguments();
-      const match = type.getText().match(regex);
-      const keyTypeName = match![1];
-      const keyType = typeArguments.find(
-        (type) => type.getText() === keyTypeName
-      )!;
-      const valueType = typeArguments.find(
-        (type) => type.getText() !== keyTypeName
-      )!;
-
-      return new MapComponent(
-        nullable,
-        typeAnalyser(keyType),
-        typeAnalyser(valueType)
-      );
-    }
-
-    if (typeType === "ArrayBuffer") {
-      return new ArrayBufferComponent(nullable);
-    }
-
-    throw new Error(
-      "Not implemented for type: " +
-        type.getSymbol()!.compilerSymbol.escapedName
-    );
-  }
-
-  return new DefaultComponent();
-}
-
 for (const sourceFile of sourceFiles) {
   console.log("Source file path: ", sourceFile.getFilePath());
   const classes = sourceFile.getClasses();
   for (const classDeclaration of classes) {
     console.log("Class: ", classDeclaration.getName());
 
-    // toJSONGenerator(classDeclaration);
-    // fromJSONGenerator(classDeclaration);
-
     const classComponents: [string, ClassComponent][] = classDeclaration
       .getProperties()
       .map((property) => {
-        let type = property.getType();
-
-        return [property.getName(), typeAnalyser(type)];
+        return [property.getName(), classComponentFactory(property.getType())];
       });
 
     const bundle = new ClassBundle(
@@ -110,7 +33,7 @@ for (const sourceFile of sourceFiles) {
     toJSONGenerator(classDeclaration, bundle);
     fromJSONGenerator(classDeclaration, bundle);
 
-    const imports = bundle.imports(sourceFile);
+    bundle.imports(sourceFile);
     sourceFile.organizeImports();
   }
   sourceFile.saveSync();
