07ab2ca4dbf0b0ba0193c12ed620cf0d36b2d3ac
Clément VANDENDAELEN <cv@icure.com>
2023-05-11 10:33:08 +0200

    CodingReference mappings

--- packages/ehr-lite/ehr/src/mappings/CodingReference.mapper.ts
+++ packages/ehr-lite/ehr/src/mappings/CodingReference.mapper.ts
@@ -1,29 +1,58 @@
-import {createMap, forMember, ignore, mapFrom} from "@automapper/core";
-import {mapper} from "./mapper";
-import {CodeStub} from "@icure/api";
-import {CodingReference} from "../models/CodingReference.model";
+import { createMap, forMember, ignore, mapFrom } from '@automapper/core'
+import { mapper } from './mapper'
+import { CodeStub } from '@icure/api'
+import { CodingReference } from '../models/CodingReference.model'
 
-export const initializeCodingReferenceMapper = () => {
-    createMap(
-        mapper,
-        CodeStub,
-        CodingReference,
-        forMember(c => c.id, mapFrom(c => c.id)),
-        forMember(c => c.type, mapFrom(c => c.type)),
-        forMember(c => c.code, mapFrom(c => c.code)),
-        forMember(c => c.version, mapFrom(c => c.version)),
-        forMember(c => c.label, mapFrom(c => !!c.label ? new Map(Object.entries(c.label)) : undefined)),
-    )
+function forMember_CodeStub_id() {
+    return forMember<CodingReference, CodeStub>(v => v.id, mapFrom(v => `${v.type ?? null}|${v.code ?? null}|${v.version ?? null}`))
+}
 
-    createMap(
-        mapper,
-        CodingReference,
-        CodeStub,
-        forMember(c => c.id, mapFrom(c => `${c.type ?? null}|${c.code ?? null}|${c.version ?? null}`)),
-        forMember(c => c.context, ignore()),
-        forMember(c => c.type, mapFrom(c => c.type)),
-        forMember(c => c.code, mapFrom(c => c.code)),
-        forMember(c => c.version, mapFrom(c => c.version)),
-        forMember(c => c.label, mapFrom(c => !!c.label ? Object.fromEntries(c.label) : undefined)),
-    )
+function forMember_CodeStub_context() {
+    return forMember<CodingReference, CodeStub>(v => v.context, mapFrom(v => v.context))
+}
+
+function forMember_CodeStub_type() {
+    return forMember<CodingReference, CodeStub>(v => v.type, mapFrom(v => v.type))
+}
+
+function forMember_CodeStub_code() {
+    return forMember<CodingReference, CodeStub>(v => v.code, mapFrom(v => v.code))
+}
+
+function forMember_CodeStub_version() {
+    return forMember<CodingReference, CodeStub>(v => v.version, mapFrom(v => v.version))
+}
+
+function forMember_CodeStub_label() {
+    return forMember<CodingReference, CodeStub>(v => v.label, mapFrom(v => (!!v.label ? Object.fromEntries(v.label.entries()) : undefined)))
+}
+
+function forMember_CodingReference_id() {
+    return forMember<CodeStub, CodingReference>(v => v.id, mapFrom(v => `${v.type ?? null}|${v.code ?? null}|${v.version ?? null}`))
+}
+
+function forMember_CodingReference_type() {
+    return forMember<CodeStub, CodingReference>(v => v.type, mapFrom(v => v.type))
+}
+
+function forMember_CodingReference_code() {
+    return forMember<CodeStub, CodingReference>(v => v.code, mapFrom(v => v.code))
+}
+
+function forMember_CodingReference_version() {
+    return forMember<CodeStub, CodingReference>(v => v.version, mapFrom(v => v.version))
+}
+
+function forMember_CodingReference_label() {
+    return forMember<CodeStub, CodingReference>(v => v.label, mapFrom(v => (!!v.label ? new Map(Object.entries(v.label)) : undefined)))
+}
+
+export function initializeCodingReferenceMapper() {
+    createMap(mapper, CodingReference, CodeStub, forMember_CodeStub_id(), forMember_CodeStub_context(), forMember_CodeStub_type(), forMember_CodeStub_code(), forMember_CodeStub_version(), forMember_CodeStub_label())
+
+    createMap(mapper, CodeStub, CodingReference, forMember_CodingReference_id(), forMember_CodingReference_type(), forMember_CodingReference_code(), forMember_CodingReference_context(), forMember_CodingReference_version(), forMember_CodingReference_label())
+}
+
+function forMember_CodingReference_context() {
+    return forMember<CodeStub, CodingReference>(v => v.context, mapFrom(v => v.context))
 }
--- packages/ehr-lite/ehr/src/models/CodingReference.model.ts
+++ packages/ehr-lite/ehr/src/models/CodingReference.model.ts
@@ -1,40 +1,40 @@
-import { ISO639_1 } from "@icure/api";
+import { CodeStub, ISO639_1 } from '@icure/api'
+import { mapTo } from '../mappings/mapper'
 
+@mapTo(CodeStub)
 export class CodingReference {
-  id?: string;
-  type?: string;
-  code?: string;
-  version?: string;
-  label?: Map<ISO639_1, string>;
+    id?: string
+    type?: string
+    code?: string
+    context?: string
+    version?: string
+    label?: Map<ISO639_1, string>
 
-  static toJSON(instance: CodingReference): any {
-    const pojo: any = {};
-    pojo["id"] = instance.id;
-    pojo["type"] = instance.type;
-    pojo["code"] = instance.code;
-    pojo["version"] = instance.version;
-    pojo["label"] = !!instance.label
-      ? Object.fromEntries(
-          [...instance.label.entries()].map(([k, v]) => [k, v])
-        )
-      : undefined;
-    return pojo;
-  }
+    static toJSON(instance: CodingReference): any {
+        const pojo: any = {}
+        pojo["id"] = instance.id
+        pojo["type"] = instance.type
+        pojo["code"] = instance.code
+        pojo["context"] = instance.context
+        pojo["version"] = instance.version
+        pojo["label"] = !!instance.label ? Object.fromEntries([...instance.label.entries()].map(([k, v]) => [k, v])) : undefined
+        return pojo
+    }
 
-  static fromJSON(pojo: any): CodingReference {
-    const instance = new CodingReference();
-    if (pojo["id"] === undefined) instance.id = undefined;
-    instance.id = pojo["id"];
-    if (pojo["type"] === undefined) instance.type = undefined;
-    instance.type = pojo["type"];
-    if (pojo["code"] === undefined) instance.code = undefined;
-    instance.code = pojo["code"];
-    if (pojo["version"] === undefined) instance.version = undefined;
-    instance.version = pojo["version"];
-    if (pojo["label"] === undefined) instance.label = undefined;
-    instance.label = pojo["label"]
-      ? new Map(pojo["label"].map(([k, v]: [any, any]) => [k, v]))
-      : undefined;
-    return instance;
-  }
+    static fromJSON(pojo: any): CodingReference {
+        const instance = new CodingReference()
+        if (pojo["id"] === undefined) instance.id = undefined
+        instance.id = pojo["id"]
+        if (pojo["type"] === undefined) instance.type = undefined
+        instance.type = pojo["type"]
+        if (pojo["code"] === undefined) instance.code = undefined
+        instance.code = pojo["code"]
+        if (pojo["context"] === undefined) instance.context = undefined
+        instance.context = pojo["context"]
+        if (pojo["version"] === undefined) instance.version = undefined
+        instance.version = pojo["version"]
+        if (pojo["label"] === undefined) instance.label = undefined
+        instance.label = pojo["label"] ? new Map(pojo["label"].map(([k, v]: [any, any]) => [k, v])) : undefined
+        return instance
+    }
 }
