f6a2f3bf3256bfa6aa523dffb1b3a385905905e7
Clément VANDENDAELEN <cv@icure.com>
2023-05-11 16:56:41 +0200

    Identifier mappings

--- packages/ehr-lite/ehr/src/mappings/Identifier.mapper.ts
+++ packages/ehr-lite/ehr/src/mappings/Identifier.mapper.ts
@@ -1,36 +1,76 @@
-import {createMap, forMember, mapFrom, mapWith} from "@automapper/core";
-import {mapper} from "./mapper";
-import {Identifier as IdentifierEntity} from "@icure/api/icc-api/model/Identifier";
-import {Identifier} from "../models/Identifier.model";
-import {CodeStub} from "@icure/api";
-import {CodingReference} from "../models/CodingReference.model";
+import { createMap, forMember, mapFrom, mapWith } from '@automapper/core'
+import { mapper } from './mapper'
+import { Identifier as IdentifierEntity } from '@icure/api/icc-api/model/Identifier'
+import { Identifier } from '../models/Identifier.model'
+import { CodeStub } from '@icure/api'
+import { CodingReference } from '../models/CodingReference.model'
 
-export const initializeIdentifierMapper = () => {
-    createMap(
-        mapper,
-        IdentifierEntity,
-        Identifier,
-        forMember(i => i.id, mapFrom(i => i.id)),
-        forMember(i => i.assigner, mapFrom(i => i.assigner)),
-        forMember(i => i.start, mapFrom(i => i.start)),
-        forMember(i => i.end, mapFrom(i => i.end)),
-        forMember(i => i.system, mapFrom(i => i.system)),
-        forMember(i => i.type, mapWith(CodeStub, CodingReference, i => i.type)),
-        forMember(i => i.use, mapFrom(i => i.use)),
-        forMember(i => i.value, mapFrom(i => i.value)),
-    )
+function forMember_IdentifierEntity_id() {
+    return forMember<Identifier, IdentifierEntity>(v => v.id, mapFrom(v => v.id))
+}
 
-    createMap(
-        mapper,
-        Identifier,
-        IdentifierEntity,
-        forMember(i => i.id, mapFrom(i => i.id)),
-        forMember(i => i.assigner, mapFrom(i => i.assigner)),
-        forMember(i => i.start, mapFrom(i => i.start)),
-        forMember(i => i.end, mapFrom(i => i.end)),
-        forMember(i => i.system, mapFrom(i => i.system)),
-        forMember(i => i.type, mapWith(CodingReference, CodeStub, i => i.type)),
-        forMember(i => i.use, mapFrom(i => i.use)),
-        forMember(i => i.value, mapFrom(i => i.value)),
-    )
+function forMember_IdentifierEntity_assigner() {
+    return forMember<Identifier, IdentifierEntity>(v => v.assigner, mapFrom(v => v.assigner))
+}
+
+function forMember_IdentifierEntity_start() {
+    return forMember<Identifier, IdentifierEntity>(v => v.start, mapFrom(v => v.start))
+}
+
+function forMember_IdentifierEntity_end() {
+    return forMember<Identifier, IdentifierEntity>(v => v.end, mapFrom(v => v.end))
+}
+
+function forMember_IdentifierEntity_system() {
+    return forMember<Identifier, IdentifierEntity>(v => v.system, mapFrom(v => v.system))
+}
+
+function forMember_IdentifierEntity_type() {
+    return forMember<Identifier, IdentifierEntity>(v => v.type, mapWith(CodeStub, CodingReference, v => v.type))
+}
+
+function forMember_IdentifierEntity_use() {
+    return forMember<Identifier, IdentifierEntity>(v => v.use, mapFrom(v => v.use))
+}
+
+function forMember_IdentifierEntity_value() {
+    return forMember<Identifier, IdentifierEntity>(v => v.value, mapFrom(v => v.value))
+}
+
+function forMember_Identifier_assigner() {
+    return forMember<IdentifierEntity, Identifier>(v => v.assigner, mapFrom(v => v.assigner))
+}
+
+function forMember_Identifier_end() {
+    return forMember<IdentifierEntity, Identifier>(v => v.end, mapFrom(v => v.end))
+}
+
+function forMember_Identifier_id() {
+    return forMember<IdentifierEntity, Identifier>(v => v.id, mapFrom(v => v.id))
+}
+
+function forMember_Identifier_start() {
+    return forMember<IdentifierEntity, Identifier>(v => v.start, mapFrom(v => v.start))
+}
+
+function forMember_Identifier_system() {
+    return forMember<IdentifierEntity, Identifier>(v => v.system, mapFrom(v => v.system))
+}
+
+function forMember_Identifier_type() {
+    return forMember<IdentifierEntity, Identifier>(v => v.type, mapWith(CodingReference, CodeStub, v => v.type))
+}
+
+function forMember_Identifier_use() {
+    return forMember<IdentifierEntity, Identifier>(v => v.use, mapFrom(v => v.use))
+}
+
+function forMember_Identifier_value() {
+    return forMember<IdentifierEntity, Identifier>(v => v.value, mapFrom(v => v.value))
+}
+
+export function initializeIdentifierMapper() {
+    createMap(mapper, Identifier, IdentifierEntity, forMember_IdentifierEntity_id(), forMember_IdentifierEntity_assigner(), forMember_IdentifierEntity_start(), forMember_IdentifierEntity_end(), forMember_IdentifierEntity_system(), forMember_IdentifierEntity_type(), forMember_IdentifierEntity_use(), forMember_IdentifierEntity_value())
+
+    createMap(mapper, IdentifierEntity, Identifier, forMember_Identifier_assigner(), forMember_Identifier_end(), forMember_Identifier_id(), forMember_Identifier_start(), forMember_Identifier_system(), forMember_Identifier_type(), forMember_Identifier_use(), forMember_Identifier_value())
 }
--- packages/ehr-lite/ehr/src/models/Identifier.model.ts
+++ packages/ehr-lite/ehr/src/models/Identifier.model.ts
@@ -1,61 +1,54 @@
-import { CodingReference } from "./CodingReference.model";
+import { Identifier as IdentifierEntity } from '@icure/api'
+import { mapTo } from '../mappings/mapper'
+import { CodingReference } from './CodingReference.model'
 
-interface IIdentifier {
-  id?: string;
-  assigner?: string;
-  start?: string;
-  end?: string;
-  system?: string;
-  type?: CodingReference;
-  use?: string;
-  value?: string;
+@mapTo(IdentifierEntity)
+export class Identifier {
+    assigner?: string
+    end?: string
+    id?: string
+    start?: string
+    system?: string
+    type?: CodingReference
+    use?: string
+    value?: string
+
+    constructor(identifier: IIdentifier | any) {
+        this.assigner = identifier.assigner
+        this.end = identifier.end
+        this.id = identifier.id
+        this.start = identifier.start
+        this.system = identifier.system
+        this.type = identifier.type
+        this.use = identifier.use
+        this.value = identifier.value
+    }
+
+    static toJSON(instance: Identifier): any {
+        const pojo: any = {}
+        pojo["assigner"] = instance.assigner
+        pojo["end"] = instance.end
+        pojo["id"] = instance.id
+        pojo["start"] = instance.start
+        pojo["system"] = instance.system
+        pojo["type"] = !!instance.type ? CodingReference.toJSON(instance.type) : undefined
+        pojo["use"] = instance.use
+        pojo["value"] = instance.value
+        return pojo
+    }
+
+    static fromJSON(pojo: any): Identifier {
+        return new Identifier({assigner: pojo["assigner"], end: pojo["end"], id: pojo["id"], start: pojo["start"], system: pojo["system"], type: !!pojo["type"] ? CodingReference.fromJSON(pojo["type"]) : undefined, use: pojo["use"], value: pojo["value"]})
+    }
 }
 
-export class Identifier implements IIdentifier {
-  assigner?: string;
-  end?: string;
-  id?: string;
-  start?: string;
-  system?: string;
-  type?: CodingReference;
-  use?: string;
-  value?: string;
-
-  static toJSON(instance: Identifier): any {
-    const pojo: any = {};
-    pojo["assigner"] = instance.assigner;
-    pojo["end"] = instance.end;
-    pojo["id"] = instance.id;
-    pojo["start"] = instance.start;
-    pojo["system"] = instance.system;
-    pojo["type"] = !!instance.type
-      ? CodingReference.toJSON(instance.type)
-      : undefined;
-    pojo["use"] = instance.use;
-    pojo["value"] = instance.value;
-    return pojo;
-  }
-
-  static fromJSON(pojo: any): Identifier {
-    const instance = new Identifier();
-    if (pojo["assigner"] === undefined) instance.assigner = undefined;
-    instance.assigner = pojo["assigner"];
-    if (pojo["end"] === undefined) instance.end = undefined;
-    instance.end = pojo["end"];
-    if (pojo["id"] === undefined) instance.id = undefined;
-    instance.id = pojo["id"];
-    if (pojo["start"] === undefined) instance.start = undefined;
-    instance.start = pojo["start"];
-    if (pojo["system"] === undefined) instance.system = undefined;
-    instance.system = pojo["system"];
-    if (pojo["type"] === undefined) instance.type = undefined;
-    instance.type = !!pojo["type"]
-      ? CodingReference.fromJSON(pojo["type"])
-      : undefined;
-    if (pojo["use"] === undefined) instance.use = undefined;
-    instance.use = pojo["use"];
-    if (pojo["value"] === undefined) instance.value = undefined;
-    instance.value = pojo["value"];
-    return instance;
-  }
+interface IIdentifier {
+    assigner?: string
+    end?: string
+    id?: string
+    start?: string
+    system?: string
+    type?: CodingReference
+    use?: string
+    value?: string
 }
