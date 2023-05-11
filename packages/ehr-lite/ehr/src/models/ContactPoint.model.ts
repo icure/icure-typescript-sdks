2bd5d65a09eac62849d24fc12ee91057ca490dda
Clément VANDENDAELEN <cv@icure.com>
2023-05-11 16:33:09 +0200

    ContactPoint mappings

--- packages/ehr-lite/ehr/src/models/ContactPoint.model.ts
+++ packages/ehr-lite/ehr/src/models/ContactPoint.model.ts
@@ -1,26 +1,37 @@
-import { ContactPointTelecomTypeEnum } from "./enums/ContactPointTelecomType.enum";
+import { Telecom } from '@icure/api'
+import { mapTo } from '../mappings/mapper'
+import { ContactPointTelecomTypeEnum } from './enums/ContactPointTelecomType.enum'
 
+@mapTo(Telecom)
 export class ContactPoint {
-  system?: ContactPointTelecomTypeEnum;
-  value?: string;
-  description?: string;
+    system?: ContactPointTelecomTypeEnum
+    value?: string
+    description?: string
+    encryptedSelf?: string;
 
-  static toJSON(instance: ContactPoint): any {
-    const pojo: any = {};
-    pojo["system"] = instance.system;
-    pojo["value"] = instance.value;
-    pojo["description"] = instance.description;
-    return pojo;
-  }
+    constructor(contactPoint: IContactPoint | any) {
+        this.system = contactPoint.system
+        this.value = contactPoint.value
+        this.description = contactPoint.description
+    }
 
-  static fromJSON(pojo: any): ContactPoint {
-    const instance = new ContactPoint();
-    if (pojo["system"] === undefined) instance.system = undefined;
-    instance.system = pojo["system"];
-    if (pojo["value"] === undefined) instance.value = undefined;
-    instance.value = pojo["value"];
-    if (pojo["description"] === undefined) instance.description = undefined;
-    instance.description = pojo["description"];
-    return instance;
-  }
+    static toJSON(instance: ContactPoint): any {
+        const pojo: any = {}
+        pojo["system"] = instance.system
+        pojo["value"] = instance.value
+        pojo["description"] = instance.description
+        pojo["encryptedSelf"] = instance.encryptedSelf
+        return pojo
+    }
+
+    static fromJSON(pojo: any): ContactPoint {
+        return new ContactPoint({system: pojo["system"], value: pojo["value"], description: pojo["description"], encryptedSelf: pojo["encryptedSelf"]})
+    }
+}
+
+interface IContactPoint {
+    system?: ContactPointTelecomTypeEnum
+    value?: string
+    description?: string
+    encryptedSelf?: string;
 }
