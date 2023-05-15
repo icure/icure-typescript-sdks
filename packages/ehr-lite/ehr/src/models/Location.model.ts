b63361aeb1d117a17b07d1a645e97585d1adb04a
Clément VANDENDAELEN <cv@icure.com>
2023-05-15 11:55:20 +0200

    Location mappings

--- packages/ehr-lite/ehr/src/models/Location.model.ts
+++ packages/ehr-lite/ehr/src/models/Location.model.ts
@@ -1,66 +1,70 @@
-import { Annotation } from "./Annotation.model";
-import { ContactPoint } from "./ContactPoint.model";
-import { LocationAddressTypeEnum } from "./enums/LocationAddressType.enum";
+import { Address } from '@icure/api'
+import { mapTo } from '../mappings/mapper'
+import { Annotation } from './Annotation.model'
+import { ContactPoint } from './ContactPoint.model'
+import { LocationAddressTypeEnum } from './enums/LocationAddressType.enum'
 
+@mapTo(Address)
 export class Location {
-  addressType?: LocationAddressTypeEnum;
-  description?: string;
-  street?: string;
-  houseNumber?: string;
-  postboxNumber?: string;
-  postalCode?: string;
-  city?: string;
-  state?: string;
-  country?: string;
-  notes?: Annotation[];
-  telecoms?: ContactPoint[];
+    addressType?: LocationAddressTypeEnum
+    description?: string
+    street?: string
+    houseNumber?: string
+    postboxNumber?: string
+    postalCode?: string
+    city?: string
+    state?: string
+    country?: string
+    notes?: Annotation[]
+    telecoms?: ContactPoint[]
+    encryptedSelf?: string;
 
-  static toJSON(instance: Location): any {
-    const pojo: any = {};
-    pojo["addressType"] = instance.addressType;
-    pojo["description"] = instance.description;
-    pojo["street"] = instance.street;
-    pojo["houseNumber"] = instance.houseNumber;
-    pojo["postboxNumber"] = instance.postboxNumber;
-    pojo["postalCode"] = instance.postalCode;
-    pojo["city"] = instance.city;
-    pojo["state"] = instance.state;
-    pojo["country"] = instance.country;
-    pojo["notes"] = instance.notes?.map((item) => Annotation.toJSON(item));
-    pojo["telecoms"] = instance.telecoms?.map((item) =>
-      ContactPoint.toJSON(item)
-    );
-    return pojo;
-  }
+    constructor(location: ILocation | any) {
+        this.addressType = location.addressType
+        this.description = location.description
+        this.street = location.street
+        this.houseNumber = location.houseNumber
+        this.postboxNumber = location.postboxNumber
+        this.postalCode = location.postalCode
+        this.city = location.city
+        this.state = location.state
+        this.country = location.country
+        this.notes = location.notes
+        this.telecoms = location.telecoms
+    }
 
-  static fromJSON(pojo: any): Location {
-    const instance = new Location();
-    if (pojo["addressType"] === undefined) instance.addressType = undefined;
-    instance.addressType = pojo["addressType"];
-    if (pojo["description"] === undefined) instance.description = undefined;
-    instance.description = pojo["description"];
-    if (pojo["street"] === undefined) instance.street = undefined;
-    instance.street = pojo["street"];
-    if (pojo["houseNumber"] === undefined) instance.houseNumber = undefined;
-    instance.houseNumber = pojo["houseNumber"];
-    if (pojo["postboxNumber"] === undefined) instance.postboxNumber = undefined;
-    instance.postboxNumber = pojo["postboxNumber"];
-    if (pojo["postalCode"] === undefined) instance.postalCode = undefined;
-    instance.postalCode = pojo["postalCode"];
-    if (pojo["city"] === undefined) instance.city = undefined;
-    instance.city = pojo["city"];
-    if (pojo["state"] === undefined) instance.state = undefined;
-    instance.state = pojo["state"];
-    if (pojo["country"] === undefined) instance.country = undefined;
-    instance.country = pojo["country"];
-    if (pojo["notes"] === undefined) instance.notes = undefined;
-    instance.notes = pojo["notes"]?.map((item: any) =>
-      Annotation.fromJSON(item)
-    );
-    if (pojo["telecoms"] === undefined) instance.telecoms = undefined;
-    instance.telecoms = pojo["telecoms"]?.map((item: any) =>
-      ContactPoint.fromJSON(item)
-    );
-    return instance;
-  }
+    static toJSON(instance: Location): any {
+        const pojo: any = {}
+        pojo["addressType"] = instance.addressType
+        pojo["description"] = instance.description
+        pojo["street"] = instance.street
+        pojo["houseNumber"] = instance.houseNumber
+        pojo["postboxNumber"] = instance.postboxNumber
+        pojo["postalCode"] = instance.postalCode
+        pojo["city"] = instance.city
+        pojo["state"] = instance.state
+        pojo["country"] = instance.country
+        pojo["notes"] = instance.notes?.map(item => Annotation.toJSON(item))
+        pojo["telecoms"] = instance.telecoms?.map(item => ContactPoint.toJSON(item))
+        pojo["encryptedSelf"] = instance.encryptedSelf
+        return pojo
+    }
+
+    static fromJSON(pojo: any): Location {
+        return new Location({addressType: pojo["addressType"], description: pojo["description"], street: pojo["street"], houseNumber: pojo["houseNumber"], postboxNumber: pojo["postboxNumber"], postalCode: pojo["postalCode"], city: pojo["city"], state: pojo["state"], country: pojo["country"], notes: pojo["notes"]?.map((item: any) => Annotation.fromJSON(item)), telecoms: pojo["telecoms"]?.map((item: any) => ContactPoint.fromJSON(item)), encryptedSelf: pojo["encryptedSelf"]})
+    }
+}
+
+interface ILocation {
+    addressType?: LocationAddressTypeEnum
+    description?: string
+    street?: string
+    houseNumber?: string
+    postboxNumber?: string
+    postalCode?: string
+    city?: string
+    state?: string
+    country?: string
+    notes?: Annotation[]
+    telecoms?: ContactPoint[]
 }
