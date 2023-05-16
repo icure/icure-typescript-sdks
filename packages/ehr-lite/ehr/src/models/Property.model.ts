4595fabb2ba6bc7685b68e1dea7985bbe10869c6
Clément VANDENDAELEN <cv@icure.com>
2023-05-16 15:46:11 +0200

    Property mappings

--- packages/ehr-lite/ehr/src/models/Property.model.ts
+++ packages/ehr-lite/ehr/src/models/Property.model.ts
@@ -1,39 +1,42 @@
-import { PropertyType } from "./PropertyType.model";
-import { TypedValueObject } from "./TypedValueObject.model";
+import { PropertyStub } from '@icure/api'
+import { mapTo } from '../mappings/mapper'
+import { PropertyType } from './PropertyType.model'
+import { TypedValueObject } from './TypedValueObject.model'
 
+@mapTo(PropertyStub)
 export class Property {
-  id?: string;
-  type?: PropertyType;
-  typedValue?: TypedValueObject;
-  deleted?: number;
+    id?: string
+    type?: PropertyType
+    typedValue?: TypedValueObject
+    deleted?: number
+    encryptedSelf?: string
 
-  static toJSON(instance: Property): any {
-    const pojo: any = {};
-    pojo["id"] = instance.id;
-    pojo["type"] = !!instance.type
-      ? PropertyType.toJSON(instance.type)
-      : undefined;
-    pojo["typedValue"] = !!instance.typedValue
-      ? TypedValueObject.toJSON(instance.typedValue)
-      : undefined;
-    pojo["deleted"] = instance.deleted;
-    return pojo;
-  }
+    constructor(property: IProperty | any) {
+        this.id = property.id
+        this.type = property.type
+        this.typedValue = property.typedValue
+        this.deleted = property.deleted
+    }
 
-  static fromJSON(pojo: any): Property {
-    const instance = new Property();
-    if (pojo["id"] === undefined) instance.id = undefined;
-    instance.id = pojo["id"];
-    if (pojo["type"] === undefined) instance.type = undefined;
-    instance.type = !!pojo["type"]
-      ? PropertyType.fromJSON(pojo["type"])
-      : undefined;
-    if (pojo["typedValue"] === undefined) instance.typedValue = undefined;
-    instance.typedValue = !!pojo["typedValue"]
-      ? TypedValueObject.fromJSON(pojo["typedValue"])
-      : undefined;
-    if (pojo["deleted"] === undefined) instance.deleted = undefined;
-    instance.deleted = pojo["deleted"];
-    return instance;
-  }
+    static toJSON(instance: Property): any {
+        const pojo: any = {}
+        pojo['id'] = instance.id
+        pojo['type'] = !!instance.type ? PropertyType.toJSON(instance.type) : undefined
+        pojo['typedValue'] = !!instance.typedValue ? TypedValueObject.toJSON(instance.typedValue) : undefined
+        pojo['deleted'] = instance.deleted
+        pojo['encryptedSelf'] = instance.encryptedSelf
+        return pojo
+    }
+
+    static fromJSON(pojo: any): Property {
+        return new Property({ id: pojo['id'], type: !!pojo['type'] ? PropertyType.fromJSON(pojo['type']) : undefined, typedValue: !!pojo['typedValue'] ? TypedValueObject.fromJSON(pojo['typedValue']) : undefined, deleted: pojo['deleted'], encryptedSelf: pojo['encryptedSelf'] })
+    }
+}
+
+interface IProperty {
+    id?: string
+    type?: PropertyType
+    typedValue?: TypedValueObject
+    deleted?: number
+    encryptedSelf?: string
 }
