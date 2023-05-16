185de4be82d4f4fa181e509c27847576f1f37f79
Clément VANDENDAELEN <cv@icure.com>
2023-05-16 15:47:33 +0200

    PropertyType mappings

--- packages/ehr-lite/ehr/src/models/PropertyType.model.ts
+++ packages/ehr-lite/ehr/src/models/PropertyType.model.ts
@@ -1,22 +1,30 @@
-import { TypeEnum } from "./enums/Type.enum";
+import { PropertyTypeStub } from '@icure/api'
+import { mapTo } from '../mappings/mapper'
+import { TypeEnum } from './enums/Type.enum'
 
+@mapTo(PropertyTypeStub)
 export class PropertyType {
-  identifier?: string;
-  type?: TypeEnum;
+    identifier?: string
+    type?: TypeEnum
 
-  static toJSON(instance: PropertyType): any {
-    const pojo: any = {};
-    pojo["identifier"] = instance.identifier;
-    pojo["type"] = instance.type;
-    return pojo;
-  }
+    constructor(propertyType: IPropertyType | any) {
+        this.identifier = propertyType.identifier
+        this.type = propertyType.type
+    }
 
-  static fromJSON(pojo: any): PropertyType {
-    const instance = new PropertyType();
-    if (pojo["identifier"] === undefined) instance.identifier = undefined;
-    instance.identifier = pojo["identifier"];
-    if (pojo["type"] === undefined) instance.type = undefined;
-    instance.type = pojo["type"];
-    return instance;
-  }
+    static toJSON(instance: PropertyType): any {
+        const pojo: any = {}
+        pojo['identifier'] = instance.identifier
+        pojo['type'] = instance.type
+        return pojo
+    }
+
+    static fromJSON(pojo: any): PropertyType {
+        return new PropertyType({ identifier: pojo['identifier'], type: pojo['type'] })
+    }
+}
+
+interface IPropertyType {
+    identifier?: string
+    type?: TypeEnum
 }
