6ad93306137266f3d1beeed1d44ba93ce556dd68
Clément VANDENDAELEN <cv@icure.com>
2023-05-16 17:05:25 +0200

    TimeSeries mappings

---
+++ packages/ehr-lite/ehr/src/mappings/TimeSeries.mapper.ts
@@ -0,0 +1,119 @@
+import { TimeSeries } from '../models/TimeSeries.model'
+import { TimeSeries as TimeSeriesEntity } from '@icure/api'
+import { createMap, forMember, mapFrom } from '@automapper/core'
+import { mapper } from './mapper'
+
+function forMember_TimeSeriesEntity_fields() {
+    return forMember<TimeSeries, TimeSeriesEntity>(
+        (v) => v.fields,
+        mapFrom((t) => t.fields)
+    )
+}
+
+function forMember_TimeSeriesEntity_samples() {
+    return forMember<TimeSeries, TimeSeriesEntity>(
+        (v) => v.samples,
+        mapFrom((t) => t.samples)
+    )
+}
+
+function forMember_TimeSeriesEntity_min() {
+    return forMember<TimeSeries, TimeSeriesEntity>(
+        (v) => v.min,
+        mapFrom((t) => t.min)
+    )
+}
+
+function forMember_TimeSeriesEntity_max() {
+    return forMember<TimeSeries, TimeSeriesEntity>(
+        (v) => v.max,
+        mapFrom((t) => t.max)
+    )
+}
+
+function forMember_TimeSeriesEntity_mean() {
+    return forMember<TimeSeries, TimeSeriesEntity>(
+        (v) => v.mean,
+        mapFrom((t) => t.mean)
+    )
+}
+
+function forMember_TimeSeriesEntity_median() {
+    return forMember<TimeSeries, TimeSeriesEntity>(
+        (v) => v.median,
+        mapFrom((t) => t.median)
+    )
+}
+
+function forMember_TimeSeriesEntity_variance() {
+    return forMember<TimeSeries, TimeSeriesEntity>(
+        (v) => v.variance,
+        mapFrom((t) => t.variance)
+    )
+}
+
+function forMember_TimeSeries_fields() {
+    return forMember<TimeSeriesEntity, TimeSeries>(
+        (v) => v.fields,
+        mapFrom((t) => t.fields)
+    )
+}
+
+function forMember_TimeSeries_samples() {
+    return forMember<TimeSeriesEntity, TimeSeries>(
+        (v) => v.samples,
+        mapFrom((t) => t.samples)
+    )
+}
+
+function forMember_TimeSeries_min() {
+    return forMember<TimeSeriesEntity, TimeSeries>(
+        (v) => v.min,
+        mapFrom((t) => t.min)
+    )
+}
+
+function forMember_TimeSeries_max() {
+    return forMember<TimeSeriesEntity, TimeSeries>(
+        (v) => v.max,
+        mapFrom((t) => t.max)
+    )
+}
+
+function forMember_TimeSeries_mean() {
+    return forMember<TimeSeriesEntity, TimeSeries>(
+        (v) => v.mean,
+        mapFrom((t) => t.mean)
+    )
+}
+
+function forMember_TimeSeries_median() {
+    return forMember<TimeSeriesEntity, TimeSeries>(
+        (v) => v.median,
+        mapFrom((t) => t.median)
+    )
+}
+
+function forMember_TimeSeries_variance() {
+    return forMember<TimeSeriesEntity, TimeSeries>(
+        (v) => v.variance,
+        mapFrom((t) => t.variance)
+    )
+}
+
+export function initializeTimeSeriesMapper() {
+    createMap(
+        mapper,
+        TimeSeries,
+        TimeSeriesEntity,
+        forMember_TimeSeriesEntity_fields(),
+        forMember_TimeSeriesEntity_samples(),
+        forMember_TimeSeriesEntity_min(),
+        forMember_TimeSeriesEntity_max(),
+        forMember_TimeSeriesEntity_mean(),
+        forMember_TimeSeriesEntity_median(),
+        forMember_TimeSeriesEntity_variance()
+    )
+
+    createMap(mapper, TimeSeriesEntity, TimeSeries, forMember_TimeSeries_fields(), forMember_TimeSeries_samples(), forMember_TimeSeries_min(), forMember_TimeSeries_max(), forMember_TimeSeries_mean(), forMember_TimeSeries_median(), forMember_TimeSeries_variance())
+}
--- packages/ehr-lite/ehr/src/models/TimeSeries.model.ts
+++ packages/ehr-lite/ehr/src/models/TimeSeries.model.ts
@@ -1,42 +1,57 @@
+import { TimeSeries as TimeSeriesEntity } from '@icure/api'
+import { mapTo } from '../mappings/mapper'
+
+@mapTo(TimeSeriesEntity)
 export class TimeSeries {
-  fields?: string[];
-  samples?: number[][];
-  min?: number[];
-  max?: number[];
-  mean?: number[];
-  median?: number[];
-  variance?: number[];
+    fields?: string[]
+    samples?: number[][]
+    min?: number[]
+    max?: number[]
+    mean?: number[]
+    median?: number[]
+    variance?: number[]
 
-  static toJSON(instance: TimeSeries): any {
-    const pojo: any = {};
-    pojo["fields"] = instance.fields?.map((item) => item);
-    pojo["samples"] = instance.samples?.map((item) => item.map((item) => item));
-    pojo["min"] = instance.min?.map((item) => item);
-    pojo["max"] = instance.max?.map((item) => item);
-    pojo["mean"] = instance.mean?.map((item) => item);
-    pojo["median"] = instance.median?.map((item) => item);
-    pojo["variance"] = instance.variance?.map((item) => item);
-    return pojo;
-  }
+    constructor(timeSeries: ITimeSeries | any) {
+        this.fields = timeSeries.fields
+        this.samples = timeSeries.samples
+        this.min = timeSeries.min
+        this.max = timeSeries.max
+        this.mean = timeSeries.mean
+        this.median = timeSeries.median
+        this.variance = timeSeries.variance
+    }
 
-  static fromJSON(pojo: any): TimeSeries {
-    const instance = new TimeSeries();
-    if (pojo["fields"] === undefined) instance.fields = undefined;
-    instance.fields = pojo["fields"]?.map((item: any) => item);
-    if (pojo["samples"] === undefined) instance.samples = undefined;
-    instance.samples = pojo["samples"]?.map((item: any) =>
-      item.map((item: any) => item)
-    );
-    if (pojo["min"] === undefined) instance.min = undefined;
-    instance.min = pojo["min"]?.map((item: any) => item);
-    if (pojo["max"] === undefined) instance.max = undefined;
-    instance.max = pojo["max"]?.map((item: any) => item);
-    if (pojo["mean"] === undefined) instance.mean = undefined;
-    instance.mean = pojo["mean"]?.map((item: any) => item);
-    if (pojo["median"] === undefined) instance.median = undefined;
-    instance.median = pojo["median"]?.map((item: any) => item);
-    if (pojo["variance"] === undefined) instance.variance = undefined;
-    instance.variance = pojo["variance"]?.map((item: any) => item);
-    return instance;
-  }
+    static toJSON(instance: TimeSeries): any {
+        const pojo: any = {}
+        pojo['fields'] = instance.fields?.map((item) => item)
+        pojo['samples'] = instance.samples?.map((item) => item.map((item) => item))
+        pojo['min'] = instance.min?.map((item) => item)
+        pojo['max'] = instance.max?.map((item) => item)
+        pojo['mean'] = instance.mean?.map((item) => item)
+        pojo['median'] = instance.median?.map((item) => item)
+        pojo['variance'] = instance.variance?.map((item) => item)
+        return pojo
+    }
+
+    static fromJSON(pojo: any): TimeSeries {
+        return new TimeSeries({
+            fields: pojo['fields']?.map((item: any) => item),
+            samples: pojo['samples']?.map((item: any) => item.map((item: any) => item)),
+            min: pojo['min']?.map((item: any) => item),
+            max: pojo['max']?.map((item: any) => item),
+            mean: pojo['mean']?.map((item: any) => item),
+            median: pojo['median']?.map((item: any) => item),
+            variance: pojo['variance']?.map((item: any) => item),
+        })
+    }
+}
+
+export interface ITimeSeries {
+    fields?: string[]
+    samples?: number[][]
+    min?: number[]
+    max?: number[]
+    mean?: number[]
+    median?: number[]
+    variance?: number[]
 }
