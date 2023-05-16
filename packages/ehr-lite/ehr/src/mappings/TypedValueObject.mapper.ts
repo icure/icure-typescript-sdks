8486b1f8d0cc72d7ef819bae8fe52db399d8ad6e
Clément VANDENDAELEN <cv@icure.com>
2023-05-16 17:07:47 +0200

    TypedValueObject mappings

---
+++ packages/ehr-lite/ehr/src/mappings/TypedValueObject.mapper.ts
@@ -0,0 +1,130 @@
+import { TypedValueObject } from '../models/TypedValueObject.model'
+import { TypedValueObject as TypedValueObjectEntity } from '@icure/api'
+import { createMap, forMember, mapFrom } from '@automapper/core'
+import { mapper } from './mapper'
+
+function forMember_TypedValueObjectEntity_type() {
+    return forMember<TypedValueObject, TypedValueObjectEntity>(
+        (v) => v.type,
+        mapFrom((t) => t.type)
+    )
+}
+
+function forMember_TypedValueObjectEntity_booleanValue() {
+    return forMember<TypedValueObject, TypedValueObjectEntity>(
+        (v) => v.booleanValue,
+        mapFrom((t) => t.booleanValue)
+    )
+}
+
+function forMember_TypedValueObjectEntity_integerValue() {
+    return forMember<TypedValueObject, TypedValueObjectEntity>(
+        (v) => v.integerValue,
+        mapFrom((t) => t.integerValue)
+    )
+}
+
+function forMember_TypedValueObjectEntity_doubleValue() {
+    return forMember<TypedValueObject, TypedValueObjectEntity>(
+        (v) => v.doubleValue,
+        mapFrom((t) => t.doubleValue)
+    )
+}
+
+function forMember_TypedValueObjectEntity_stringValue() {
+    return forMember<TypedValueObject, TypedValueObjectEntity>(
+        (v) => v.stringValue,
+        mapFrom((t) => t.stringValue)
+    )
+}
+
+function forMember_TypedValueObjectEntity_dateValue() {
+    return forMember<TypedValueObject, TypedValueObjectEntity>(
+        (v) => v.dateValue,
+        mapFrom((t) => t.dateValue)
+    )
+}
+
+function forMember_TypedValueObjectEntity_encryptedSelf() {
+    return forMember<TypedValueObject, TypedValueObjectEntity>(
+        (v) => v.encryptedSelf,
+        mapFrom((t) => t.encryptedSelf)
+    )
+}
+
+function forMember_TypedValueObject_type() {
+    return forMember<TypedValueObjectEntity, TypedValueObject>(
+        (v) => v.type,
+        mapFrom((t) => t.type)
+    )
+}
+
+function forMember_TypedValueObject_booleanValue() {
+    return forMember<TypedValueObjectEntity, TypedValueObject>(
+        (v) => v.booleanValue,
+        mapFrom((t) => t.booleanValue)
+    )
+}
+
+function forMember_TypedValueObject_integerValue() {
+    return forMember<TypedValueObjectEntity, TypedValueObject>(
+        (v) => v.integerValue,
+        mapFrom((t) => t.integerValue)
+    )
+}
+
+function forMember_TypedValueObject_doubleValue() {
+    return forMember<TypedValueObjectEntity, TypedValueObject>(
+        (v) => v.doubleValue,
+        mapFrom((t) => t.doubleValue)
+    )
+}
+
+function forMember_TypedValueObject_stringValue() {
+    return forMember<TypedValueObjectEntity, TypedValueObject>(
+        (v) => v.stringValue,
+        mapFrom((t) => t.stringValue)
+    )
+}
+
+function forMember_TypedValueObject_dateValue() {
+    return forMember<TypedValueObjectEntity, TypedValueObject>(
+        (v) => v.dateValue,
+        mapFrom((t) => t.dateValue)
+    )
+}
+
+function forMember_TypedValueObject_encryptedSelf() {
+    return forMember<TypedValueObjectEntity, TypedValueObject>(
+        (v) => v.encryptedSelf,
+        mapFrom((t) => t.encryptedSelf)
+    )
+}
+
+export function initializeTypedValueObjectMapper() {
+    createMap(
+        mapper,
+        TypedValueObject,
+        TypedValueObjectEntity,
+        forMember_TypedValueObjectEntity_type(),
+        forMember_TypedValueObjectEntity_booleanValue(),
+        forMember_TypedValueObjectEntity_integerValue(),
+        forMember_TypedValueObjectEntity_doubleValue(),
+        forMember_TypedValueObjectEntity_stringValue(),
+        forMember_TypedValueObjectEntity_dateValue(),
+        forMember_TypedValueObjectEntity_encryptedSelf()
+    )
+
+    createMap(
+        mapper,
+        TypedValueObjectEntity,
+        TypedValueObject,
+        forMember_TypedValueObject_type(),
+        forMember_TypedValueObject_booleanValue(),
+        forMember_TypedValueObject_integerValue(),
+        forMember_TypedValueObject_doubleValue(),
+        forMember_TypedValueObject_stringValue(),
+        forMember_TypedValueObject_dateValue(),
+        forMember_TypedValueObject_encryptedSelf()
+    )
+}
--- packages/ehr-lite/ehr/src/models/TypedValueObject.model.ts
+++ packages/ehr-lite/ehr/src/models/TypedValueObject.model.ts
@@ -1,38 +1,50 @@
-import { TypeEnum } from "./enums/Type.enum";
+import { TypedValueObject as TypedValueObjectEntity } from '@icure/api'
+import { mapTo } from '../mappings/mapper'
+import { TypeEnum } from './enums/Type.enum'
 
+@mapTo(TypedValueObjectEntity)
 export class TypedValueObject {
-  type?: TypeEnum;
-  booleanValue?: boolean;
-  integerValue?: number;
-  doubleValue?: number;
-  stringValue?: string;
-  dateValue?: number;
+    type?: TypeEnum
+    booleanValue?: boolean
+    integerValue?: number
+    doubleValue?: number
+    stringValue?: string
+    dateValue?: number
+    encryptedSelf?: string
 
-  static toJSON(instance: TypedValueObject): any {
-    const pojo: any = {};
-    pojo["type"] = instance.type;
-    pojo["booleanValue"] = instance.booleanValue;
-    pojo["integerValue"] = instance.integerValue;
-    pojo["doubleValue"] = instance.doubleValue;
-    pojo["stringValue"] = instance.stringValue;
-    pojo["dateValue"] = instance.dateValue;
-    return pojo;
-  }
+    constructor(typedValueObject: ITypedValueObject | any) {
+        this.type = typedValueObject.type
+        this.booleanValue = typedValueObject.booleanValue
+        this.integerValue = typedValueObject.integerValue
+        this.doubleValue = typedValueObject.doubleValue
+        this.stringValue = typedValueObject.stringValue
+        this.dateValue = typedValueObject.dateValue
+        this.encryptedSelf = typedValueObject.encryptedSelf
+    }
 
-  static fromJSON(pojo: any): TypedValueObject {
-    const instance = new TypedValueObject();
-    if (pojo["type"] === undefined) instance.type = undefined;
-    instance.type = pojo["type"];
-    if (pojo["booleanValue"] === undefined) instance.booleanValue = undefined;
-    instance.booleanValue = pojo["booleanValue"];
-    if (pojo["integerValue"] === undefined) instance.integerValue = undefined;
-    instance.integerValue = pojo["integerValue"];
-    if (pojo["doubleValue"] === undefined) instance.doubleValue = undefined;
-    instance.doubleValue = pojo["doubleValue"];
-    if (pojo["stringValue"] === undefined) instance.stringValue = undefined;
-    instance.stringValue = pojo["stringValue"];
-    if (pojo["dateValue"] === undefined) instance.dateValue = undefined;
-    instance.dateValue = pojo["dateValue"];
-    return instance;
-  }
+    static toJSON(instance: TypedValueObject): any {
+        const pojo: any = {}
+        pojo['type'] = instance.type
+        pojo['booleanValue'] = instance.booleanValue
+        pojo['integerValue'] = instance.integerValue
+        pojo['doubleValue'] = instance.doubleValue
+        pojo['stringValue'] = instance.stringValue
+        pojo['dateValue'] = instance.dateValue
+        pojo['encryptedSelf'] = instance.encryptedSelf
+        return pojo
+    }
+
+    static fromJSON(pojo: any): TypedValueObject {
+        return new TypedValueObject({ type: pojo['type'], booleanValue: pojo['booleanValue'], integerValue: pojo['integerValue'], doubleValue: pojo['doubleValue'], stringValue: pojo['stringValue'], dateValue: pojo['dateValue'], encryptedSelf: pojo['encryptedSelf'] })
+    }
+}
+
+interface ITypedValueObject {
+    type?: TypeEnum
+    booleanValue?: boolean
+    integerValue?: number
+    doubleValue?: number
+    stringValue?: string
+    dateValue?: number
+    encryptedSelf?: string
 }
