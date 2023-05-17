7cd1efef4eda2b95e062a82c64f0b10c8aa2d5d3
Clément VANDENDAELEN <cv@icure.com>
2023-05-17 13:35:00 +0200

    Fixed test compilation issues

---
+++ packages/ehr-lite/ehr/babel.config.js
@@ -0,0 +1,8 @@
+module.exports = (api) => {
+    const isTest = api.env('test')
+
+    return {
+        presets: [['@babel/preset-env', { targets: { node: 'current' } }], '@babel/preset-typescript'],
+        plugins: [['@babel/plugin-proposal-decorators', { version: '2023-01' }]],
+    }
+}
--- packages/ehr-lite/ehr/src/mappings/mapper.ts
+++ packages/ehr-lite/ehr/src/mappings/mapper.ts
@@ -2,27 +2,27 @@ import { classes } from '@automapper/classes'
 import { CamelCaseNamingConvention, createMapper } from '@automapper/core'
 import { initializeAnnotationMapper } from './Annotation.mapper'
 import { initializeCodingReferenceMapper } from './CodingReference.mapper'
-import { initializeComponentMapper } from "./Component.mapper"
+import { initializeComponentMapper } from './Component.mapper'
 import { initializeConditionMapper } from './Condition.mapper'
-import { initializeContactPointMapper } from "./ContactPoint.mapper"
-import { initializeDelegationMapper } from "./Delegation.mapper"
-import { initializeHumanNameMapper } from "./HumanName.mapper"
+import { initializeContactPointMapper } from './ContactPoint.mapper'
+import { initializeDelegationMapper } from './Delegation.mapper'
+import { initializeHumanNameMapper } from './HumanName.mapper'
 import { initializeIdentifierMapper } from './Identifier.mapper'
-import { initializeLocalComponentMapper } from "./LocalComponent.mapper"
-import { initializeLocationMapper } from "./Location.mapper"
-import { initializeMeasureMapper } from "./Measure.mapper"
-import { initializeObservationMapper } from "./Observation.mapper"
-import { initializeOrganisationMapper } from "./Organisation.mapper"
-import { initializePatientMapper } from "./Patient.mapper"
-import { initializePractitionerMapper } from "./Practitioner.mapper"
-import { initializePropertyMapper } from "./Property.mapper"
-import { initializePropertyTypeMapper } from "./PropertyType.mapper"
-import { initializeRelatedPersonMapper } from "./RelatedPerson.mapper"
-import { initializeRelatedPractitionerMapper } from "./RelatedPractitioner.mapper"
-import { initializeSecureDelegationMapper } from "./SecureDelegation.mapper"
-import { initializeSecurityMetadataMapper } from "./SecurityMetadata.mapper"
-import { initializeTimeSeriesMapper } from "./TimeSeries.mapper"
-import { initializeTypedValueObjectMapper } from "./TypedValueObject.mapper"
+import { initializeLocalComponentMapper } from './LocalComponent.mapper'
+import { initializeLocationMapper } from './Location.mapper'
+import { initializeMeasureMapper } from './Measure.mapper'
+import { initializeObservationMapper } from './Observation.mapper'
+import { initializeOrganisationMapper } from './Organisation.mapper'
+import { initializePatientMapper } from './Patient.mapper'
+import { initializePractitionerMapper } from './Practitioner.mapper'
+import { initializePropertyMapper } from './Property.mapper'
+import { initializePropertyTypeMapper } from './PropertyType.mapper'
+import { initializeRelatedPersonMapper } from './RelatedPerson.mapper'
+import { initializeRelatedPractitionerMapper } from './RelatedPractitioner.mapper'
+import { initializeSecureDelegationMapper } from './SecureDelegation.mapper'
+import { initializeSecurityMetadataMapper } from './SecurityMetadata.mapper'
+import { initializeTimeSeriesMapper } from './TimeSeries.mapper'
+import { initializeTypedValueObjectMapper } from './TypedValueObject.mapper'
 
 export const mapper = createMapper({
     strategyInitializer: classes(),
