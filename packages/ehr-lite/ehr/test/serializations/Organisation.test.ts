0411bf4b70a1acc9301e307d81b48f90b3adecc6
Clément VANDENDAELEN <cv@icure.com>
2023-05-25 17:00:21 +0200

    Added missing Organisation model generator and test
    Signed-off-by: Clément VANDENDAELEN <cv@icure.com>

---
+++ packages/ehr-lite/ehr/test/serializations/Organisation.test.ts
@@ -0,0 +1,13 @@
+import {Organisation} from "../../src/models/Organisation.model";
+import {generateOrganisation} from "../models/Organisation.model";
+
+describe(`Organisation serialization and deserialization`, () => {
+    it('should correctly serialize and deserialize from instance to JSON and back', () => {
+        const instance = generateOrganisation()
+
+        const json = Organisation.toJSON(instance)
+        const newInstance = Organisation.fromJSON(json)
+
+        expect(newInstance).toEqual(instance)
+    })
+})
