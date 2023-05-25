e2c2badd1988b1ec34336ed48b5df277057eae9e
Clément VANDENDAELEN <cv@icure.com>
2023-05-25 13:27:36 +0200

    Code cleanup
    Signed-off-by: Clément VANDENDAELEN <cv@icure.com>

--- packages/ehr-lite/ehr/test/serializations/utils.ts
+++ packages/ehr-lite/ehr/test/serializations/utils.ts
@@ -16,34 +16,28 @@ export function generateRandomNumberArray(): number[] {
 
 export function generateRandomNumberMatrix(): number[][] {
     return Array.from({ length: 5 }, () => generateRandomNumberArray())
-} // Function to generate a random TypeEnum
-// Function to generate a random boolean
+}
+
 export function generateRandomBoolean(): boolean {
     return Math.random() < 0.5
 }
 
-// Function to generate a random integer
 export function generateRandomInteger(): number {
     return Math.floor(Math.random() * 100)
 }
 
-// Function to generate a random double
 export function generateRandomDouble(): number {
     return Math.random() * 100
 }
 
-// Function to generate a random string
 export function generateRandomString(): string {
     return Math.random().toString(36).substring(7)
 }
 
-// Function to generate a random date as a number (timestamp)
 export function generateRandomDate(): number {
     return new Date().getTime()
 }
 
-// Function to generate a random encryptedSelf
 export function generateRandomEncryptedSelf(): string {
-    // This is a placeholder. Replace this with the appropriate logic to generate a valid encryptedSelf
     return generateRandomString()
 }
