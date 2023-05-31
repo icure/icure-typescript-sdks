38198c59b7e9314bec51bd77ff46db6f2ea8b8cf
Clément VANDENDAELEN <cv@icure.com>
2023-05-31 14:35:43 +0200

    Initial commit

---
+++ packages/hephaestus/src/marshallerGenerator.ts
@@ -0,0 +1,33 @@
+import { Project } from 'ts-morph'
+import { ClassComponent } from './types/ClassComponent'
+import { classComponentFactory } from './generators/classComponentFactory'
+import { ClassBundle } from './types/ClassBundle'
+import { toJSONGenerator } from './generators/toJSONGenerator'
+import { fromJSONGenerator } from './generators/fromJSONGenerator'
+import { modelRegex } from './index'
+
+export function marshallerGenerator(project: Project) {
+    const sourceFiles = project.getSourceFiles().filter((sourceFile) => {
+        return modelRegex.test(sourceFile.getFilePath())
+    })
+
+    for (const sourceFile of sourceFiles) {
+        const classes = sourceFile.getClasses()
+        for (const classDeclaration of classes) {
+            console.log('Marshalling ', classDeclaration.getName())
+
+            const classComponents: [string, ClassComponent][] = classDeclaration.getProperties().map((property) => {
+                return [property.getName(), classComponentFactory(property.getType())]
+            })
+
+            const bundle = new ClassBundle(classDeclaration.getName()!, new Map(classComponents))
+
+            toJSONGenerator(classDeclaration, bundle)
+            fromJSONGenerator(classDeclaration, bundle)
+
+            bundle.imports(sourceFile)
+            sourceFile.organizeImports()
+        }
+        sourceFile.saveSync()
+    }
+}
---
+++ packages/hephaestus/src/types/ClassEntityComponent.ts
@@ -0,0 +1,22 @@
+import { ClassComponent } from "./ClassComponent";
+
+export class ClassEntityComponent extends ClassComponent {
+  readonly name: string;
+
+  constructor(name: string, nullable: boolean) {
+    super(nullable);
+    this.name = name;
+  }
+
+  computeDeserializer(value: string): string {
+    return this.nullable
+      ? `!!${value} ? ${this.name}.fromJSON(${value}) : undefined`
+      : `${this.name}.fromJSON(${value})`;
+  }
+
+  computeSerializer(value: string): string {
+    return this.nullable
+      ? `!!${value} ? ${this.name}.toJSON(${value}) : undefined`
+      : `${this.name}.toJSON(${value})`;
+  }
+}
