b52981d1a6bcdb77ee60e2a817c74694d753e9b9
Clément VANDENDAELEN <cv@icure.com>
2023-07-14 13:47:29 +0200

    Fixed SystemMetaData models nullable fields

--- packages/common/src/models/SystemMetaDataOwner.model.ts
+++ packages/common/src/models/SystemMetaDataOwner.model.ts
@@ -2,43 +2,44 @@ import { CodingReference } from './CodingReference.model'
 
 export class SystemMetaDataOwner {
     publicKey?: string
-    hcPartyKeys?: Map<string, string[]>
-    privateKeyShamirPartitions?: Map<string, string>
-    aesExchangeKeys?: Map<string, Map<string, Map<string, string>>>
-    transferKeys?: Map<string, Map<string, string>>
-    publicKeysForOaepWithSha256?: Array<string>
-    tags?: Set<CodingReference>
+    hcPartyKeys: Map<string, string[]>
+    privateKeyShamirPartitions: Map<string, string>
+    aesExchangeKeys: Map<string, Map<string, Map<string, string>>>
+    transferKeys: Map<string, Map<string, string>>
+    publicKeysForOaepWithSha256: Array<string>
+    tags: Set<CodingReference>
 
-    constructor(systemMetaDataOwner?: ISystemMetaDataOwner) {
-        this.publicKey = systemMetaDataOwner?.publicKey
-        this.hcPartyKeys = systemMetaDataOwner?.hcPartyKeys
-        this.privateKeyShamirPartitions = systemMetaDataOwner?.privateKeyShamirPartitions
-        this.aesExchangeKeys = systemMetaDataOwner?.aesExchangeKeys
-        this.transferKeys = systemMetaDataOwner?.transferKeys
-        this.publicKeysForOaepWithSha256 = systemMetaDataOwner?.publicKeysForOaepWithSha256
+    constructor(systemMetaDataOwner: ISystemMetaDataOwner) {
+        this.publicKey = systemMetaDataOwner.publicKey
+        this.hcPartyKeys = systemMetaDataOwner.hcPartyKeys ?? new Map<string, string[]>()
+        this.privateKeyShamirPartitions = systemMetaDataOwner.privateKeyShamirPartitions ?? new Map<string, string>()
+        this.aesExchangeKeys = systemMetaDataOwner.aesExchangeKeys ?? new Map<string, Map<string, Map<string, string>>>()
+        this.transferKeys = systemMetaDataOwner.transferKeys ?? new Map<string, Map<string, string>>()
+        this.publicKeysForOaepWithSha256 = systemMetaDataOwner.publicKeysForOaepWithSha256 ?? []
+        this.tags = systemMetaDataOwner.tags ?? new Set<CodingReference>()
     }
 
     static toJSON(instance: SystemMetaDataOwner): any {
         const pojo: any = {}
         pojo['publicKey'] = instance.publicKey
-        pojo['hcPartyKeys'] = !!instance.hcPartyKeys ? Object.fromEntries([...instance.hcPartyKeys.entries()].map(([k, v]) => [k, v.map((item) => item)])) : undefined
-        pojo['privateKeyShamirPartitions'] = !!instance.privateKeyShamirPartitions ? Object.fromEntries([...instance.privateKeyShamirPartitions.entries()].map(([k, v]) => [k, v])) : undefined
-        pojo['aesExchangeKeys'] = !!instance.aesExchangeKeys ? Object.fromEntries([...instance.aesExchangeKeys.entries()].map(([k, v]) => [k, Object.fromEntries([...v.entries()].map(([k, v]) => [k, Object.fromEntries([...v.entries()].map(([k, v]) => [k, v]))]))])) : undefined
-        pojo['transferKeys'] = !!instance.transferKeys ? Object.fromEntries([...instance.transferKeys.entries()].map(([k, v]) => [k, Object.fromEntries([...v.entries()].map(([k, v]) => [k, v]))])) : undefined
-        pojo['publicKeysForOaepWithSha256'] = instance.publicKeysForOaepWithSha256?.map((item) => item)
-        pojo['tags'] = Array.from([...(instance.tags ?? [])]?.map((item) => CodingReference.toJSON(item)) ?? [])
+        pojo['hcPartyKeys'] = Object.fromEntries([...instance.hcPartyKeys.entries()].map(([k, v]) => [k, v.map((item) => item)]))
+        pojo['privateKeyShamirPartitions'] = Object.fromEntries([...instance.privateKeyShamirPartitions.entries()].map(([k, v]) => [k, v]))
+        pojo['aesExchangeKeys'] = Object.fromEntries([...instance.aesExchangeKeys.entries()].map(([k, v]) => [k, Object.fromEntries([...v.entries()].map(([k, v]) => [k, Object.fromEntries([...v.entries()].map(([k, v]) => [k, v]))]))]))
+        pojo['transferKeys'] = Object.fromEntries([...instance.transferKeys.entries()].map(([k, v]) => [k, Object.fromEntries([...v.entries()].map(([k, v]) => [k, v]))]))
+        pojo['publicKeysForOaepWithSha256'] = instance.publicKeysForOaepWithSha256.map((item) => item)
+        pojo['tags'] = Array.from([...instance.tags].map((item) => CodingReference.toJSON(item)))
         return pojo
     }
 
     static fromJSON(pojo: any): SystemMetaDataOwner {
         return new SystemMetaDataOwner({
             publicKey: pojo['publicKey'],
-            hcPartyKeys: pojo['hcPartyKeys'] ? new Map(Object.entries(pojo['hcPartyKeys']).map(([k, v]: [any, any]) => [k, v.map((item: any) => item)])) : undefined,
-            privateKeyShamirPartitions: pojo['privateKeyShamirPartitions'] ? new Map(Object.entries(pojo['privateKeyShamirPartitions']).map(([k, v]: [any, any]) => [k, v])) : undefined,
-            aesExchangeKeys: pojo['aesExchangeKeys'] ? new Map(Object.entries(pojo['aesExchangeKeys']).map(([k, v]: [any, any]) => [k, new Map(Object.entries(v).map(([k, v]: [any, any]) => [k, new Map(Object.entries(v).map(([k, v]: [any, any]) => [k, v]))]))])) : undefined,
-            transferKeys: pojo['transferKeys'] ? new Map(Object.entries(pojo['transferKeys']).map(([k, v]: [any, any]) => [k, new Map(Object.entries(v).map(([k, v]: [any, any]) => [k, v]))])) : undefined,
-            publicKeysForOaepWithSha256: pojo['publicKeysForOaepWithSha256']?.map((item: any) => item),
-            tags: new Set(pojo['tags']?.map((item: any) => CodingReference.fromJSON(item)) ?? []),
+            hcPartyKeys: new Map(Object.entries(pojo['hcPartyKeys']).map(([k, v]: [any, any]) => [k, v.map((item: any) => item)])),
+            privateKeyShamirPartitions: new Map(Object.entries(pojo['privateKeyShamirPartitions']).map(([k, v]: [any, any]) => [k, v])),
+            aesExchangeKeys: new Map(Object.entries(pojo['aesExchangeKeys']).map(([k, v]: [any, any]) => [k, new Map(Object.entries(v).map(([k, v]: [any, any]) => [k, new Map(Object.entries(v).map(([k, v]: [any, any]) => [k, v]))]))])),
+            transferKeys: new Map(Object.entries(pojo['transferKeys']).map(([k, v]: [any, any]) => [k, new Map(Object.entries(v).map(([k, v]: [any, any]) => [k, v]))])),
+            publicKeysForOaepWithSha256: pojo['publicKeysForOaepWithSha256'].map((item: any) => item),
+            tags: new Set(pojo['tags'].map((item: any) => CodingReference.fromJSON(item))),
         })
     }
 }
--- packages/common/src/models/SystemMetaDataOwnerEncrypted.model.ts
+++ packages/common/src/models/SystemMetaDataOwnerEncrypted.model.ts
@@ -4,67 +4,68 @@ import { SecurityMetadata } from './SecurityMetadata.model'
 
 export class SystemMetaDataOwnerEncrypted {
     publicKey?: string
-    hcPartyKeys?: Map<string, string[]>
-    privateKeyShamirPartitions?: Map<string, string>
-    secretForeignKeys?: string[]
-    cryptedForeignKeys?: Map<string, Set<Delegation>>
-    delegations?: Map<string, Set<Delegation>>
-    encryptionKeys?: Map<string, Set<Delegation>>
-    aesExchangeKeys?: Map<string, Map<string, Map<string, string>>>
-    transferKeys?: Map<string, Map<string, string>>
+    hcPartyKeys: Map<string, string[]>
+    privateKeyShamirPartitions: Map<string, string>
+    secretForeignKeys: string[]
+    cryptedForeignKeys: Map<string, Set<Delegation>>
+    delegations: Map<string, Set<Delegation>>
+    encryptionKeys: Map<string, Set<Delegation>>
+    aesExchangeKeys: Map<string, Map<string, Map<string, string>>>
+    transferKeys: Map<string, Map<string, string>>
     securityMetadata?: SecurityMetadata
-    publicKeysForOaepWithSha256?: Array<string>
+    publicKeysForOaepWithSha256: Array<string>
     encryptedSelf?: string
-    tags?: Set<CodingReference>
+    tags: Set<CodingReference>
 
-    constructor(systemMetaDataOwnerEncrypted?: ISystemMetaDataOwnerEncrypted) {
-        this.publicKey = systemMetaDataOwnerEncrypted?.publicKey
-        this.hcPartyKeys = systemMetaDataOwnerEncrypted?.hcPartyKeys
-        this.privateKeyShamirPartitions = systemMetaDataOwnerEncrypted?.privateKeyShamirPartitions
-        this.secretForeignKeys = systemMetaDataOwnerEncrypted?.secretForeignKeys
-        this.cryptedForeignKeys = systemMetaDataOwnerEncrypted?.cryptedForeignKeys
-        this.delegations = systemMetaDataOwnerEncrypted?.delegations
-        this.encryptionKeys = systemMetaDataOwnerEncrypted?.encryptionKeys
-        this.aesExchangeKeys = systemMetaDataOwnerEncrypted?.aesExchangeKeys
-        this.transferKeys = systemMetaDataOwnerEncrypted?.transferKeys
-        this.securityMetadata = systemMetaDataOwnerEncrypted?.securityMetadata
-        this.encryptedSelf = systemMetaDataOwnerEncrypted?.encryptedSelf
-        this.publicKeysForOaepWithSha256 = systemMetaDataOwnerEncrypted?.publicKeysForOaepWithSha256
+    constructor(systemMetaDataOwnerEncrypted: ISystemMetaDataOwnerEncrypted) {
+        this.publicKey = systemMetaDataOwnerEncrypted.publicKey
+        this.hcPartyKeys = systemMetaDataOwnerEncrypted.hcPartyKeys ?? new Map<string, string[]>()
+        this.privateKeyShamirPartitions = systemMetaDataOwnerEncrypted.privateKeyShamirPartitions ?? new Map<string, string>()
+        this.secretForeignKeys = systemMetaDataOwnerEncrypted.secretForeignKeys ?? []
+        this.cryptedForeignKeys = systemMetaDataOwnerEncrypted.cryptedForeignKeys ?? new Map<string, Set<Delegation>>()
+        this.delegations = systemMetaDataOwnerEncrypted.delegations ?? new Map<string, Set<Delegation>>()
+        this.encryptionKeys = systemMetaDataOwnerEncrypted.encryptionKeys ?? new Map<string, Set<Delegation>>()
+        this.aesExchangeKeys = systemMetaDataOwnerEncrypted.aesExchangeKeys ?? new Map<string, Map<string, Map<string, string>>>()
+        this.transferKeys = systemMetaDataOwnerEncrypted.transferKeys ?? new Map<string, Map<string, string>>()
+        this.securityMetadata = systemMetaDataOwnerEncrypted.securityMetadata
+        this.encryptedSelf = systemMetaDataOwnerEncrypted.encryptedSelf
+        this.publicKeysForOaepWithSha256 = systemMetaDataOwnerEncrypted.publicKeysForOaepWithSha256 ?? []
+        this.tags = systemMetaDataOwnerEncrypted.tags ?? new Set<CodingReference>()
     }
 
     static toJSON(instance: SystemMetaDataOwnerEncrypted): any {
         const pojo: any = {}
         pojo['publicKey'] = instance.publicKey
-        pojo['hcPartyKeys'] = !!instance.hcPartyKeys ? Object.fromEntries([...instance.hcPartyKeys.entries()].map(([k, v]) => [k, v.map((item) => item)])) : undefined
-        pojo['privateKeyShamirPartitions'] = !!instance.privateKeyShamirPartitions ? Object.fromEntries([...instance.privateKeyShamirPartitions.entries()].map(([k, v]) => [k, v])) : undefined
-        pojo['secretForeignKeys'] = instance.secretForeignKeys?.map((item) => item)
-        pojo['cryptedForeignKeys'] = !!instance.cryptedForeignKeys ? Object.fromEntries([...instance.cryptedForeignKeys.entries()].map(([k, v]) => [k, Array.from([...v].map((item) => Delegation.toJSON(item)))])) : undefined
-        pojo['delegations'] = !!instance.delegations ? Object.fromEntries([...instance.delegations.entries()].map(([k, v]) => [k, Array.from([...v].map((item) => Delegation.toJSON(item)))])) : undefined
-        pojo['encryptionKeys'] = !!instance.encryptionKeys ? Object.fromEntries([...instance.encryptionKeys.entries()].map(([k, v]) => [k, Array.from([...v].map((item) => Delegation.toJSON(item)))])) : undefined
-        pojo['aesExchangeKeys'] = !!instance.aesExchangeKeys ? Object.fromEntries([...instance.aesExchangeKeys.entries()].map(([k, v]) => [k, Object.fromEntries([...v.entries()].map(([k, v]) => [k, Object.fromEntries([...v.entries()].map(([k, v]) => [k, v]))]))])) : undefined
-        pojo['transferKeys'] = !!instance.transferKeys ? Object.fromEntries([...instance.transferKeys.entries()].map(([k, v]) => [k, Object.fromEntries([...v.entries()].map(([k, v]) => [k, v]))])) : undefined
+        pojo['hcPartyKeys'] = Object.fromEntries([...instance.hcPartyKeys.entries()].map(([k, v]) => [k, v.map((item) => item)]))
+        pojo['privateKeyShamirPartitions'] = Object.fromEntries([...instance.privateKeyShamirPartitions.entries()].map(([k, v]) => [k, v]))
+        pojo['secretForeignKeys'] = instance.secretForeignKeys.map((item) => item)
+        pojo['cryptedForeignKeys'] = Object.fromEntries([...instance.cryptedForeignKeys.entries()].map(([k, v]) => [k, Array.from([...v].map((item) => Delegation.toJSON(item)))]))
+        pojo['delegations'] = Object.fromEntries([...instance.delegations.entries()].map(([k, v]) => [k, Array.from([...v].map((item) => Delegation.toJSON(item)))]))
+        pojo['encryptionKeys'] = Object.fromEntries([...instance.encryptionKeys.entries()].map(([k, v]) => [k, Array.from([...v].map((item) => Delegation.toJSON(item)))]))
+        pojo['aesExchangeKeys'] = Object.fromEntries([...instance.aesExchangeKeys.entries()].map(([k, v]) => [k, Object.fromEntries([...v.entries()].map(([k, v]) => [k, Object.fromEntries([...v.entries()].map(([k, v]) => [k, v]))]))]))
+        pojo['transferKeys'] = Object.fromEntries([...instance.transferKeys.entries()].map(([k, v]) => [k, Object.fromEntries([...v.entries()].map(([k, v]) => [k, v]))]))
         pojo['securityMetadata'] = !!instance.securityMetadata ? SecurityMetadata.toJSON(instance.securityMetadata) : undefined
-        pojo['publicKeysForOaepWithSha256'] = instance.publicKeysForOaepWithSha256?.map((item) => item)
+        pojo['publicKeysForOaepWithSha256'] = instance.publicKeysForOaepWithSha256.map((item) => item)
         pojo['encryptedSelf'] = instance.encryptedSelf
-        pojo['tags'] = Array.from([...(instance.tags ?? [])]?.map((item) => CodingReference.toJSON(item)) ?? [])
+        pojo['tags'] = Array.from([...instance.tags].map((item) => CodingReference.toJSON(item)))
         return pojo
     }
 
     static fromJSON(pojo: any): SystemMetaDataOwnerEncrypted {
         return new SystemMetaDataOwnerEncrypted({
             publicKey: pojo['publicKey'],
-            hcPartyKeys: pojo['hcPartyKeys'] ? new Map(Object.entries(pojo['hcPartyKeys']).map(([k, v]: [any, any]) => [k, v.map((item: any) => item)])) : undefined,
-            privateKeyShamirPartitions: pojo['privateKeyShamirPartitions'] ? new Map(Object.entries(pojo['privateKeyShamirPartitions']).map(([k, v]: [any, any]) => [k, v])) : undefined,
-            secretForeignKeys: pojo['secretForeignKeys']?.map((item: any) => item),
-            cryptedForeignKeys: pojo['cryptedForeignKeys'] ? new Map(Object.entries(pojo['cryptedForeignKeys']).map(([k, v]: [any, any]) => [k, new Set(v.map((item: any) => Delegation.fromJSON(item)))])) : undefined,
-            delegations: pojo['delegations'] ? new Map(Object.entries(pojo['delegations']).map(([k, v]: [any, any]) => [k, new Set(v.map((item: any) => Delegation.fromJSON(item)))])) : undefined,
-            encryptionKeys: pojo['encryptionKeys'] ? new Map(Object.entries(pojo['encryptionKeys']).map(([k, v]: [any, any]) => [k, new Set(v.map((item: any) => Delegation.fromJSON(item)))])) : undefined,
-            aesExchangeKeys: pojo['aesExchangeKeys'] ? new Map(Object.entries(pojo['aesExchangeKeys']).map(([k, v]: [any, any]) => [k, new Map(Object.entries(v).map(([k, v]: [any, any]) => [k, new Map(Object.entries(v).map(([k, v]: [any, any]) => [k, v]))]))])) : undefined,
-            transferKeys: pojo['transferKeys'] ? new Map(Object.entries(pojo['transferKeys']).map(([k, v]: [any, any]) => [k, new Map(Object.entries(v).map(([k, v]: [any, any]) => [k, v]))])) : undefined,
+            hcPartyKeys: new Map(Object.entries(pojo['hcPartyKeys']).map(([k, v]: [any, any]) => [k, v.map((item: any) => item)])),
+            privateKeyShamirPartitions: new Map(Object.entries(pojo['privateKeyShamirPartitions']).map(([k, v]: [any, any]) => [k, v])),
+            secretForeignKeys: pojo['secretForeignKeys'].map((item: any) => item),
+            cryptedForeignKeys: new Map(Object.entries(pojo['cryptedForeignKeys']).map(([k, v]: [any, any]) => [k, new Set(v.map((item: any) => Delegation.fromJSON(item)))])),
+            delegations: new Map(Object.entries(pojo['delegations']).map(([k, v]: [any, any]) => [k, new Set(v.map((item: any) => Delegation.fromJSON(item)))])),
+            encryptionKeys: new Map(Object.entries(pojo['encryptionKeys']).map(([k, v]: [any, any]) => [k, new Set(v.map((item: any) => Delegation.fromJSON(item)))])),
+            aesExchangeKeys: new Map(Object.entries(pojo['aesExchangeKeys']).map(([k, v]: [any, any]) => [k, new Map(Object.entries(v).map(([k, v]: [any, any]) => [k, new Map(Object.entries(v).map(([k, v]: [any, any]) => [k, v]))]))])),
+            transferKeys: new Map(Object.entries(pojo['transferKeys']).map(([k, v]: [any, any]) => [k, new Map(Object.entries(v).map(([k, v]: [any, any]) => [k, v]))])),
             securityMetadata: !!pojo['securityMetadata'] ? SecurityMetadata.fromJSON(pojo['securityMetadata']) : undefined,
-            publicKeysForOaepWithSha256: pojo['publicKeysForOaepWithSha256']?.map((item: any) => item),
+            publicKeysForOaepWithSha256: pojo['publicKeysForOaepWithSha256'].map((item: any) => item),
             encryptedSelf: pojo['encryptedSelf'],
-            tags: new Set(pojo['tags']?.map((item: any) => CodingReference.fromJSON(item)) ?? []),
+            tags: new Set(pojo['tags'].map((item: any) => CodingReference.fromJSON(item))),
         })
     }
 }
