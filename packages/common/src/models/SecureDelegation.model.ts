8d134abefec7d0efe2c7e9f1692362e57573eca6
Clément VANDENDAELEN <cv@icure.com>
2023-07-14 10:25:45 +0200

    Fixed SecureDelegation.model.ts nullables fields

--- packages/common/src/models/SecureDelegation.model.ts
+++ packages/common/src/models/SecureDelegation.model.ts
@@ -1,15 +1,15 @@
 import { AccessLevelEnum } from './enums/AccessLevel.enum'
 
 export class SecureDelegation {
-    constructor(secureDelegation?: ISecureDelegation | any) {
-        this.delegator = secureDelegation?.delegator
-        this.delegate = secureDelegation?.delegate
-        this.secretIds = secureDelegation?.secretIds
-        this.encryptionKeys = secureDelegation?.encryptionKeys
-        this.owningEntityIds = secureDelegation?.owningEntityIds
-        this.parentDelegations = secureDelegation?.parentDelegations
-        this.exchangeDataId = secureDelegation?.exchangeDataId
-        this.permissions = secureDelegation?.permissions
+    constructor(secureDelegation: ISecureDelegation) {
+        this.delegator = secureDelegation.delegator
+        this.delegate = secureDelegation.delegate
+        this.secretIds = secureDelegation.secretIds
+        this.encryptionKeys = secureDelegation.encryptionKeys
+        this.owningEntityIds = secureDelegation.owningEntityIds
+        this.parentDelegations = secureDelegation.parentDelegations
+        this.exchangeDataId = secureDelegation.exchangeDataId
+        this.permissions = secureDelegation.permissions
     }
 
     /**
@@ -26,21 +26,21 @@ export class SecureDelegation {
      * Secret id of the entity holding this [SecureDelegation] (formerly `delegation`). The id will appear in plaintext in the
      * `secretForeignKeys` field of children entities.
      */
-    secretIds?: string[]
+    secretIds: Set<string>
     /**
      * Encrypted aes key used for the encryption of the entity's data (data stored in `encryptedSelf`).
      */
-    encryptionKeys?: string[]
+    encryptionKeys: Set<string>
     /**
      * Encrypted id of the entity which owns the entity holding this [SecureDelegation] (formerly `cryptedForeignKey`),
      * such as the id of the patient for a contact or healthcare element.
      */
-    owningEntityIds?: string[]
+    owningEntityIds: Set<string>
     /**
      * Key of the parent delegations in the [SecurityMetadata.secureDelegations]. Users are allowed to modify/delete
      * only [SecureDelegation] that they can directly access or any children delegations.
      */
-    parentDelegations?: string[]
+    parentDelegations: Set<string>
     /**
      * If both the [delegator] and [delegate] are explicit in this secure delegation this field will hold the id of the exchange
      * data used for the encryption of this delegation. Otherwise, this will be null.
@@ -60,10 +60,10 @@ export class SecureDelegation {
         const pojo: any = {}
         pojo['delegator'] = instance.delegator
         pojo['delegate'] = instance.delegate
-        pojo['secretIds'] = instance.secretIds?.map((item) => item)
-        pojo['encryptionKeys'] = instance.encryptionKeys?.map((item) => item)
-        pojo['owningEntityIds'] = instance.owningEntityIds?.map((item) => item)
-        pojo['parentDelegations'] = instance.parentDelegations?.map((item) => item)
+        pojo['secretIds'] = Array.from([...instance.secretIds].map((item) => item))
+        pojo['encryptionKeys'] = Array.from([...instance.encryptionKeys].map((item) => item))
+        pojo['owningEntityIds'] = Array.from([...instance.owningEntityIds].map((item) => item))
+        pojo['parentDelegations'] = Array.from([...instance.parentDelegations].map((item) => item))
         pojo['exchangeDataId'] = instance.exchangeDataId
         pojo['permissions'] = instance.permissions
         return pojo
@@ -73,10 +73,10 @@ export class SecureDelegation {
         return new SecureDelegation({
             delegator: pojo['delegator'],
             delegate: pojo['delegate'],
-            secretIds: pojo['secretIds']?.map((item: any) => item),
-            encryptionKeys: pojo['encryptionKeys']?.map((item: any) => item),
-            owningEntityIds: pojo['owningEntityIds']?.map((item: any) => item),
-            parentDelegations: pojo['parentDelegations']?.map((item: any) => item),
+            secretIds: new Set(pojo['secretIds'].map((item: any) => item)),
+            encryptionKeys: new Set(pojo['encryptionKeys'].map((item: any) => item)),
+            owningEntityIds: new Set(pojo['owningEntityIds'].map((item: any) => item)),
+            parentDelegations: new Set(pojo['parentDelegations'].map((item: any) => item)),
             exchangeDataId: pojo['exchangeDataId'],
             permissions: pojo['permissions'],
         })
@@ -86,10 +86,10 @@ export class SecureDelegation {
 interface ISecureDelegation {
     delegator?: string
     delegate?: string
-    secretIds?: string[]
-    encryptionKeys?: string[]
-    owningEntityIds?: string[]
-    parentDelegations?: string[]
+    secretIds: Set<string>
+    encryptionKeys: Set<string>
+    owningEntityIds: Set<string>
+    parentDelegations: Set<string>
     exchangeDataId?: string
     permissions: AccessLevelEnum
 }
