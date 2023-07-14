2921c1a98b8e2440c5e98b56101884bbcd2e1897
Clément VANDENDAELEN <cv@icure.com>
2023-07-14 10:02:11 +0200

    Formatting

--- packages/common/src/models/Measure.model.ts
+++ packages/common/src/models/Measure.model.ts
@@ -1,6 +1,6 @@
-import { Measure as MeasureDto } from '@icure/api';
-import { mapTo } from "../utils/decorators";
-import { CodingReference } from "./CodingReference.model";
+import { Measure as MeasureDto } from '@icure/api'
+import { mapTo } from '../utils/decorators'
+import { CodingReference } from './CodingReference.model'
 
 @mapTo(MeasureDto)
 export class Measure {
@@ -16,38 +16,50 @@ export class Measure {
     comment?: string
     comparator?: string
 
-    constructor(measure?: IMeasure | any) {
-        this.value = measure?.value
-        this.min = measure?.min
-        this.max = measure?.max
-        this.ref = measure?.ref
-        this.severity = measure?.severity
-        this.severityCode = measure?.severityCode
-        this.evolution = measure?.evolution
-        this.unit = measure?.unit
-        this.unitCodes = measure?.unitCodes
-        this.comment = measure?.comment
-        this.comparator = measure?.comparator
+    constructor(measure: IMeasure) {
+        this.value = measure.value
+        this.min = measure.min
+        this.max = measure.max
+        this.ref = measure.ref
+        this.severity = measure.severity
+        this.severityCode = measure.severityCode
+        this.evolution = measure.evolution
+        this.unit = measure.unit
+        this.unitCodes = measure.unitCodes
+        this.comment = measure.comment
+        this.comparator = measure.comparator
     }
 
     static toJSON(instance: Measure): any {
         const pojo: any = {}
-        pojo["value"] = instance.value
-        pojo["min"] = instance.min
-        pojo["max"] = instance.max
-        pojo["ref"] = instance.ref
-        pojo["severity"] = instance.severity
-        pojo["severityCode"] = instance.severityCode
-        pojo["evolution"] = instance.evolution
-        pojo["unit"] = instance.unit
-        pojo["unitCodes"] = instance.unitCodes?.map(item => CodingReference.toJSON(item))
-        pojo["comment"] = instance.comment
-        pojo["comparator"] = instance.comparator
+        pojo['value'] = instance.value
+        pojo['min'] = instance.min
+        pojo['max'] = instance.max
+        pojo['ref'] = instance.ref
+        pojo['severity'] = instance.severity
+        pojo['severityCode'] = instance.severityCode
+        pojo['evolution'] = instance.evolution
+        pojo['unit'] = instance.unit
+        pojo['unitCodes'] = instance.unitCodes?.map((item) => CodingReference.toJSON(item))
+        pojo['comment'] = instance.comment
+        pojo['comparator'] = instance.comparator
         return pojo
     }
 
     static fromJSON(pojo: any): Measure {
-        return new Measure({value: pojo["value"], min: pojo["min"], max: pojo["max"], ref: pojo["ref"], severity: pojo["severity"], severityCode: pojo["severityCode"], evolution: pojo["evolution"], unit: pojo["unit"], unitCodes: pojo["unitCodes"]?.map((item: any) => CodingReference.fromJSON(item)), comment: pojo["comment"], comparator: pojo["comparator"]})
+        return new Measure({
+            value: pojo['value'],
+            min: pojo['min'],
+            max: pojo['max'],
+            ref: pojo['ref'],
+            severity: pojo['severity'],
+            severityCode: pojo['severityCode'],
+            evolution: pojo['evolution'],
+            unit: pojo['unit'],
+            unitCodes: pojo['unitCodes']?.map((item: any) => CodingReference.fromJSON(item)),
+            comment: pojo['comment'],
+            comparator: pojo['comparator'],
+        })
     }
 }
 
